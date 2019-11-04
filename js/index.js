d369a00eae8c602432fe1627de40293b767f7f8f
JRodDvlpr <Mialienproductions@gmail.com>
2019-11-03 21:45:35 -0500

    const

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 // Your code goes here
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
 
-const body = document.querySelector('body');
-const mouseOver = document.querySelector('.intro')
-
-
-
-
+const mouseOver = document.querySelector('.intro h2')
+const downKey = document.querySelector ()
+const Wheelie = document.querySelector ()
+const dragIt = document.querySelector ()
+const pageLoad = document.querySelector ()
+const focusedMind = document.querySelector ()
+const 
 
 
 /// ###### (1) ##### ///
+destinationDivs
